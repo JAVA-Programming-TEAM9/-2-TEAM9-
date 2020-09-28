5f90abec8b8536b8ecbfee1f43c32b4c770c8f81
TaeYeong KIM <teayung0114@naver.com>
2020-09-28 13:58:58 +0900

    app

--- MyApp.java
+++ MyApp.java
@@ -1,19 +1,45 @@
 import java.util.*;
 
 /**
- * 여기에 MyApp 클래스 설명을 작성하십시오.
+ * 오늘 날짜를 입력받아서 token을 생성하여 정수 3개를 push하고, 4번 pop하는 MyApp클래스
  * 
  * @author (2018210032 김태영, 2019315020 오성빈) 
  * @version (2020.09.28)
  */
 public class MyApp
 {
+    public static Integer[] makeToken(String data)
+    {
+        Integer[] array = new Integer[3];
+        StringTokenizer st = new StringTokenizer(data, "/");
+        int i = 0;       
+        while(st.hasMoreTokens())
+        {
+            array[i] = Integer.valueOf(st.nextToken());
+            i++;
+        }  
+        return array;
+    }
+    
     public static void main (String[] args)
     {
-        //Stack<Integer> stack = new MyStack<Integer>();
-        Scanner scanner = new Scanner(System.in);
+        Stack<Integer> stack = new MyStack<Integer>();
+        Scanner scanner = new Scanner(System.in); 
         
-      
+        System.out.print("오늘 날짜를 입력하시오(년/월/일) : ");
+        String data = scanner.next();
+        Integer[] array = makeToken(data);
+        
+        System.out.println("------ Push ------");
+        
+        for(int i=0; i<3; i++)
+            stack.push(array[i]);
+        
+        System.out.println("------ Pop ------");
+        while(true)
+        {
+            
+        }
         
     }
 }
