ee702dcd8815416a481fd12a13de45f88b2ad76a
TaeYeong KIM <teayung0114@naver.com>
2020-09-28 13:07:02 +0900

    myapp

---
+++ MyApp.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/**
+ * 여기에 MyApp 클래스 설명을 작성하십시오.
+ * 
+ * @author (2018210032 김태영, 2019315020 오성빈) 
+ * @version (2020.09.28)
+ */
+public class MyApp
+{
+    public static void main (String[] args)
+    {
+        Stack<Integer> stack = new MyStack<Integer>();
+        Scanner scanner = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer
+        
+        while(true)
+        {
+            System.out.print("오늘 날짜를 입력하시오(년/월/일) : ");
+            day = scanner.nextLine();
+            
+        }
+        
+    }
+}
